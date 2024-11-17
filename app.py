63329b956a8190abf18c1554c97cccf811104f5c
Srijan Shukla <160172137+Winter-Shade@users.noreply.github.com>
2024-11-18 05:15:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -372,5 +372,7 @@ def table():
     },
     ]
     return render_template("table.html", data=data)
+    
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
